83b3679ea1bcc85b9fd4b5282c0cc647400e0851
wangkaidd <1193323668@qq.com>
2018-05-12 08:29:32 +0800

    daly demo

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php
+var $name = $_POST.tt;
+var $pwd = $_POST.pp;
+// $name = 'zs';
+echo 1;
+echo $name-----$pwd;
+echo 2;
+?>
---
+++ js/index.js
@@ -0,0 +1,16 @@
+// 书写入口函数
+$(function () {
+  $(window).on('load',function () {
+    
+  })
+function imgLocation() {
+    var box = $('.box');
+    var boxWidth = box.eq(0).width();
+    var num = Math.floor($(window).width()/boxWidth);
+    var boxArr = [];
+    box.each(function () {
+
+    })
+}
+
+});
